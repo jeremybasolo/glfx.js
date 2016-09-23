54cdd2c0844ff34aec76bc3a62014c21aa476ae9
Jeremy Basolo <jeremybasolo@akauk.com>
2016-09-23 09:43:31 +0100

    Remove premultipliedalpha option from the webgl canvas context as it creates problems on Safari

--- src/core/canvas.js
+++ src/core/canvas.js
@@ -132,7 +132,7 @@ function wrap(func) {
 exports.canvas = function() {
     var canvas = document.createElement('canvas');
     try {
-        gl = canvas.getContext('experimental-webgl', { premultipliedAlpha: false });
+        gl = canvas.getContext('experimental-webgl');
     } catch (e) {
         gl = null;
     }
